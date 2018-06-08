e3a7d6ffd5d6aaabeac5819b45f6a6680542963c
Jack Wang <jackw@splunk.com>
2018-06-08 11:29:51 +0800

    git sdk

--- test.py
+++ test.py
@@ -1,3 +1,12 @@
 print "this is test"
 print "this is change"
 print "this is another change"
+# >>> git_repo = git.get_user().get_repo("githubapitest")
+# >>> git_pr = git_repo.get_pull(2)
+# >>> git_pr.get_files()[0]
+# File(sha="cb664c4ef280f60171e1432eb4aafb90a7b2b435", filename="test.py")
+# >>> git_pr.get_issue_comments()[0].body
+# u'this is test'
+# >>> git_pr.is_merged()
+# True
+# >>> git_pr.merge()
